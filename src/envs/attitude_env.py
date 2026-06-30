3846cc7c95a0d1f7dbda18eeb726336108d6b78c
Alliyah Steele <143225106+HelloUniversePyC@users.noreply.github.com>
2026-06-30 17:36:41 -0400

    FIX: Added constraints to attitude env

--- src/envs/attitude_env.py
+++ src/envs/attitude_env.py
@@ -2,10 +2,13 @@ import gymnasium as gym
 import torch
 import numpy as np
 from omegaconf import DictConfig
-from src.env.utils import(
+from src.envs.utils import(
     random_unit_quaternion,
     omega_matrix,
-    quaternion_error
+    quaternion_error,
+    rotate_vector,
+    random_unit_vector,
+    rotate_around_axis
 )
 
 class AttitudeEnv(gym.Env):
@@ -14,38 +17,56 @@ class AttitudeEnv(gym.Env):
         """Define observation space and action space"""
         self.alpha: float = config.alpha
         self.beta: float = config.beta
+        self.f_zone_half_angle:float = np.radians(config.f_zone_half_angle)
+        self.boresight_axis: np.ndarray = config.boresight_axis
         self.tau_max: torch.tensor = config.tau_max
         self.inertia: np.ndarray = np.diag(config.inertia_tensor)
         self.dt: int = config.dt
         self.max_steps: int = config.max_steps
         self.q_target: torch.tensor = config.q_target
-        self.observation_space: gym.spaces.Box = gym.spaces.Box(low = -1.0, high = 1.0, shape = (11,))
-        self.action_space = gym.spaces.Box(low = -self.tau_max, high = self.tau_max, shape = (3,))
+        self.observation_space: gym.spaces.Box = gym.spaces.Box(low = -1.0, 
+                                                                high = 1.0, 
+                                                                shape = (11,))
+        self.action_space = gym.spaces.Box(low = -self.tau_max, 
+                                           high = self.tau_max, 
+                                           shape = (3,))
         self.success_threshold: float = config.success_threshold
         self.success_bonus: float = config.success_bonus
         self.terminated: bool = False
         self.truncated: bool = False
-
-    def reset(self, seed = None) -> np.ndarray:
+        self.q: np.ndarray = random_unit_quaternion()
+        self.omega: np.ndarray = np.zeros(3)
+        self.t: int = 0
+        self.sun_vector = random_unit_vector()
+        self.orbital_rate = config.orbital_rate
+    def reset(self, seed = None, options = {}) -> np.ndarray:
         """Return initial (objects, info)"""
         self.q: np.ndarray = random_unit_quaternion()
         self.omega: np.ndarray = np.zeros(3)
         self.t: int = 0
         self.truncated = False
         self.terminated = False
-        return self._get_obs(), {}
-    
+        self.sun_vector = random_unit_vector()
+        return self._get_obs(), self.render()   
     def step(self,action) -> tuple:
         """return (objs, reward, terminated,truncated info)"""
-        dw_dt: np.ndarray = np.linalg.inv(self.inertia) @ (action - np.cross(self.omega, (self.inertia @ self.omega))) # Newtons 2nd F=Ma for rotation
+        ang_momentum: np.ndarray = self.inertia @ self.omega
+        torque: np.ndarray = action - np.cross(self.omega, ang_momentum)
+
+        dw_dt: np.ndarray = np.linalg.solve(self.inertia, torque) #optimization
         self.omega: np.ndarray = self.omega + dw_dt*self.dt
         dq_dt: np.ndarray = 0.5*omega_matrix(self.omega) @ self.q
         self.q: np.ndarray = self.q + dq_dt*self.dt
         self.q = self.q / np.linalg.norm(self.q)
+
+        self.sun_vector = rotate_around_axis(self.sun_vector, self.orbital_rate*self.dt)
         self.t+=1
-        return self._get_obs(), self.reward(), self.terminated, self.truncated, {}
-    
-    def reward(self) -> float:
+        fzone_violated = self._check_fzone_violation()
+        if fzone_violated:
+            self.terminated = True
+        return self._get_obs(), self.reward(fzone_violated), self.terminated, self.truncated, {}
+    def reward(self, fzone_violated) -> float:
+        """Calculates reward for current episode"""
         q_error = quaternion_error(self.q, self.q_target)
         angle_error = 2 * np.arccos(np.clip(np.abs(q_error[0]), -1.0, 1.0))
         r_attitude = -angle_error
@@ -56,7 +77,13 @@ class AttitudeEnv(gym.Env):
         if self.t >= self.max_steps:
             self.truncated = True
         r_success = self.success_bonus if correct else 0.0
-        return r_attitude + r_velocity + r_success
+        r_fzone = -self.alpha*20 if fzone_violated else 0.0
+        return r_attitude + r_velocity + r_success + r_fzone
+    def _check_fzone_violation(self) -> bool:
+        """Checks forbidden zone condition"""
+        boresight_inertial = rotate_vector(self.q, self.boresight_axis)
+        boresight_project = np.dot(boresight_inertial, self.sun_vector)
+        return boresight_project > np.cos(self.f_zone_half_angle)
 
     def _get_obs(self) -> np.ndarray:
         """helper to package state into observation vector"""
@@ -65,4 +92,5 @@ class AttitudeEnv(gym.Env):
                       self.omega[0], self.omega[1],self.omega[2], 
                       e_1, e_2, e_3,e_4])
         return obs_vector
-    
+    def render(self) -> dict:
+        return {}
--- src/envs/utils.py
+++ src/envs/utils.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 def quaternion_conjugate(q) -> np.ndarray:
+    """Calculates the conjugate of a quaternion"""
     w, x, y, z = q
     return np.array([w, -x, -y, -z])
 
 def quaternion_multiply(q1, q2) -> np.ndarray:
+    """Applies standard formula to multiply two quaternions"""
     w1, x1, y1, z1 = q1
     w2, x2, y2, z2 = q2
     return np.array([
@@ -14,27 +16,55 @@ def quaternion_multiply(q1, q2) -> np.ndarray:
         w1*z2 + x1*y2 - y1*x2 + z1*w2
     ])
 def random_unit_quaternion() -> np.ndarray:
+    """Generates a random 4 dimensional unit vector"""
     q = np.random.randn(4)
     return q/np.linalg.norm(q)
 
+def random_unit_vector() -> np.ndarray:
+    """Generates a random 3 dimensional unit vector"""
+    q = np.random.randn(3)
+    return q/np.linalg.norm(q)
+
+
 def omega_matrix(omega: np.ndarray)-> np.ndarray:
+    """Turns 3 dimensional angular velocity (omega) into 4x4 skew symmetric matrix"""
     wx, wy, wz = omega
-    Omega = np.array([
+    omega_arr = np.array([
         [ 0.0, -wx,  -wy,  -wz],
         [  wx,  0.0,   wz,  -wy],
         [  wy, -wz,   0.0,   wx],
         [  wz,  wy,  -wx,   0.0]
     ])
-    return Omega
+    return omega_arr
 
 def quaternion_error(q_current: np.ndarray, q_target: np.ndarray) -> np.ndarray:
+    """Error between target and projected quaternion used in Reward function"""
     q_target_conj: np.ndarray = quaternion_conjugate(q_target)
     q_error: np.ndarray = quaternion_multiply(q_target_conj, q_current)
     return q_error
 
 def rotate_vector(q: np.ndarray, v: np.ndarray) -> np.ndarray:
+    """Function to take a vector and rotate it to the desired quaternion angle and trajectory"""
     q_v = np.array([0.0, v[0], v[1], v[2]])
     q_conj: np.ndarray = quaternion_conjugate(q)
     rotation: np.ndarray = quaternion_multiply(quaternion_multiply(q, q_v), q_conj)
     return rotation[1:]
+def rotate_around_axis(sun_vector: np.ndarray, rot_angle: float) -> np.ndarray:
+    """Rotates sun vector by one time steps orbital rotation"""
+    orbit_normal = np.array([0,0,1])
+    rot_angle_cos = np.cos(rot_angle)
+    term_1 = sun_vector* rot_angle_cos
+
+    orthog_comp = np.cross(orbit_normal, sun_vector)
+    term_2 = orthog_comp*np.sin(rot_angle)
+
+    sun_projection = np.dot(orbit_normal, sun_vector)
+    rotation_scale = 1.0 - np.cos(rot_angle)
+    total_scale = sun_projection*rotation_scale
+    term_3 = orbit_normal* total_scale
+
+    return term_1 + term_2 + term_3
+
+
+
 
